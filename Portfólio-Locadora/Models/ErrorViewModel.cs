ec49eeaa522274b831e00caf7a9b6495ad60e882
Alexandre Sevilha <alexandre.sevilha1@gmail.com>
2025-03-21 14:38:01 -0300

    criacao de projeto

---
+++ Portfólio-Locadora/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portfólio_Locadora.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
